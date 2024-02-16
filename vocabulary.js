c8d990e4893d7d57dc7b55ec6b1944d3a4d9853f
Johny_47 <145960797+IvanBliudnik@users.noreply.github.com>
2024-02-16 14:43:33 +0300

    Add files via upload

--- vocabulary.js
+++ vocabulary.js
@@ -1,5 +1,5 @@
 // VOCABULARY (словарик)
-
+// _______________хранилище данных (data)
 const words = [
   {
     original: "WhatsApp",
@@ -17,6 +17,10 @@ const words = [
     original: "Mister",
     translation: "Чувак",
   },
+  {
+    original: "Screen",
+    translation: "Экран",
+  },
 ];
 const notification = {
   start: {
@@ -24,24 +28,45 @@ const notification = {
   },
   resultMesage: {
     finishSuccessMesage: "Конец. Ты Большой Молодец",
-    finishUnSuccessMesage: "Конец. Ты Не Молодец",
+    finishUnSuccessMesage: "Конец. Ты looser",
   },
 };
 const settings = {
+  correctAnswer: 0,
+  unCorrectAnswer: 0,
   correctAnswerMinProcent: 50,
 };
-const result = {
-  correctAnswer: 0,
-};
-let unCorrectAnswer = 0;
-document.write(notification.start.greetingMessage, "<br>");
-for (let i = 0; i < words.length; i++) {
-  let answer = prompt(words[i].original).trim();
-  if (answer === words[i].translation) {
-    result.correctAnswer = 1 + result.correctAnswer;
-  } else unCorrectAnswer = ++unCorrectAnswer;
+// ______________обьявляем функции
+function showGreetingMessage(_notification) {
+  alert(_notification.start.greetingMessage);
 }
-const correctAnswerProcent = (result.correctAnswer / words.length) * 100;
-if (correctAnswerProcent > settings.correctAnswerMinProcent) {
-  document.write(notification.resultMesage.finishSuccessMesage, "<br>");
-} else document.write(notification.resultMesage.finishUnSuccessMesage, "<br>");
+function startTraining(_words, _settings) {
+  for (let i = 0; i < _words.length; i++) {
+    let answer = prompt(_words[i].original).trim();
+    document.write(`${answer}: ${answer === _words[i].translation}<br>`);
+    if (answer === words[i].translation) {
+      settings.correctAnswer = 1 + settings.correctAnswer;
+    } else settings.unCorrectAnswer = ++settings.unCorrectAnswer;
+  }
+}
+function getPercentage(_settings, _words) {
+  return (settings.correctAnswer / _words.length) * 100;
+}
+function showResults(_words, _settings, _notification) {
+  const correctAnswerProcent = getPercentage(settings, words);
+  if (correctAnswerProcent > settings.correctAnswerMinProcent) {
+    document.write(
+      `Количество правильных ответов: ${settings.correctAnswer} <br> Количество неправильных ответов: ${settings.unCorrectAnswer}<br>`
+    );
+    document.write(notification.resultMesage.finishSuccessMesage, "<br>");
+  } else {
+    document.write(
+      `Количество правильных ответов: ${settings.correctAnswer} <br> Количество неправильных ответов: ${settings.unCorrectAnswer}<br>`
+    );
+    document.write(notification.resultMesage.finishUnSuccessMesage, "<br>");
+  }
+}
+// _______________main (application)
+showGreetingMessage(notification);
+startTraining(words, settings);
+showResults(words, settings, notification);
